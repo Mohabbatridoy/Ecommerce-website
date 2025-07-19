8d9e6f50ed8d3a5e0763bac0b20db751c630737f
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2025-07-19 17:04:27 +0600

    add navbar

--- Ecom_project/urls.py
+++ Ecom_project/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns, static
 from django.conf import settings
+from app_shop.views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_shop.urls')),
+    path('', Home.as_view(), name="home"),
+    path('Shop/', include('app_shop.urls')),
     path('account/', include('app_login.urls')),
 ]
 
