367d8ce63c754340964006f6f3092b83b86b33f8
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2025-07-22 13:15:49 +0600

    cart view

--- app_order/views.py
+++ app_order/views.py
@@ -14,23 +14,29 @@ from app_shop.models import Product
 @login_required
 def add_to_cart(request, pk):
     item = get_object_or_404(Product, pk=pk)
-    order_item = Cart.objects.get_or_create(item=item, user=request.user, purchased=False)
+    print("Item:________________")
+    print(item)
+    cart_item = Cart.objects.get_or_create(item=item, user=request.user, purchased=False)
+    print("Cart Item_______________________")
+    print(cart_item)
+    print(cart_item[0])
     order_qs = Order.objects.filter(user=request.user, ordered=False)
     if order_qs.exists():
         order = order_qs[0]
+        print("If order exit----------------------------")
+        print(order)
         if order.orderitems.filter(item=item).exists():
-            order_item[0].quantity += 1
-            order_item.save()
-            messages.info(request, "this Item quantity was updated!")
-            return redirect("app_shop:home")
+            cart_item[0].quantity+=1
+            cart_item[0].save()
+            messages.info(request, "This item quantity was updated!")
+            return redirect('app_shop:home')
         else:
-            order.orderitems.add(order_item[0])
-            messages.info(request, "This item was added to your cart!")
-            return redirect("app_shop:home")
-
+            order.orderitems.add(cart_item[0])
+            messages.info(request,"this item was added to your cart!")
+            return redirect('app_shop:home')
     else:
         order = Order(user=request.user)
         order.save()
-        order.orderitems.add(order_item[0])
-        messages.info(request, 'this item was added to your cart.')
+        order.orderitems.add(cart_item[0])
+        messages.info(request, "this item was added to your cart!")
         return redirect('app_shop:home')
