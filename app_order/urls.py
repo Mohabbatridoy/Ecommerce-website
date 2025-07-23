fd72acaae9167616db24d702f933be2ef95c9031
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2025-07-23 11:34:34 +0600

    create cart page

--- app_order/urls.py
+++ app_order/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'app_order'
 
 urlpatterns = [
-    path('add/<pk>/', views.add_to_cart, name="add"),
+    path('add/<pk>/', views.Add_to_cart, name="add"),
+    path('cart/', views.cart_view, name="cart"),
 ]
--- app_order/views.py
+++ app_order/views.py
@@ -12,31 +12,39 @@ from app_shop.models import Product
 # Create your views here.
 
 @login_required
-def add_to_cart(request, pk):
+def Add_to_cart(request, pk):
     item = get_object_or_404(Product, pk=pk)
-    print("Item:________________")
-    print(item)
+
+    #Get or Create a Cart object with clicked item
     cart_item = Cart.objects.get_or_create(item=item, user=request.user, purchased=False)
-    print("Cart Item_______________________")
-    print(cart_item)
-    print(cart_item[0])
+
+    #checking whether this user has a inactive order
     order_qs = Order.objects.filter(user=request.user, ordered=False)
     if order_qs.exists():
         order = order_qs[0]
-        print("If order exit----------------------------")
-        print(order)
-        if order.orderitems.filter(item=item).exists():
-            cart_item[0].quantity+=1
-            cart_item[0].save()
+        if order.orderitems.exists():
+            cart_item[0].quantity += 1
+            cart_item.save()
             messages.info(request, "This item quantity was updated!")
             return redirect('app_shop:home')
         else:
             order.orderitems.add(cart_item[0])
-            messages.info(request,"this item was added to your cart!")
+            messages.info(request, "This item was added to your cart!")
             return redirect('app_shop:home')
     else:
         order = Order(user=request.user)
         order.save()
         order.orderitems.add(cart_item[0])
-        messages.info(request, "this item was added to your cart!")
+        messages.info(request, "This item was added to your cart!")
+        return redirect('app-shop:home')
+    
+@login_required
+def cart_view(request):
+    carts = Cart.objects.filter(user=request.user, purchased=False)
+    orders = Order.objects.filter(user=request.user, ordered=False)
+    if carts.exists() and orders.exists():
+        order = orders[0]
+        return render(request, 'app_order/cart.html', context={'carts':carts,'order':order})
+    else:
+        messages.warning(request, "You don't have any item in your cart!")
         return redirect('app_shop:home')
