ea1d1b062ec8e1a817799e378d6aa6313f60c269
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2025-08-02 18:37:55 +0600

    product quantity increase and decrease

--- app_order/urls.py
+++ app_order/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('add/<pk>/', views.Add_to_cart, name="add"),
     path('cart/', views.cart_view, name="cart"),
     path('remove/<pk>', views.remove_from_cart, name="remove"),
-
+    path('increase/<pk>', views.increase_cart, name="increase"),
+    path('decrease/<pk>', views.decrease_item, name="decrease"),
 ]
--- app_order/views.py
+++ app_order/views.py
@@ -68,3 +68,50 @@ def remove_from_cart(request, pk):
     else:
         messages.info(request, "Your Cart is empty!")
         return redirect('app_shop:home')
+    
+
+@login_required
+def increase_cart(request, pk):
+    item = get_object_or_404(Product, pk=pk)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.orderitems.filter(item=item).exists():
+            cart_item = Cart.objects.filter(item=item, user=request.user, purchased=False)[0]
+            if cart_item.quantity >= 1:
+                cart_item.quantity += 1
+                cart_item.save()
+                messages.info(request, f"{item.name} quantity was updated!")
+                return redirect('app_order:cart')
+        else:
+            messages.info(request, f"{item.name} is not in your cart!")
+            return redirect('app_shop:home')
+    else:
+        messages.info(request, "You don't have an active order!")
+        return redirect('app_shop:home')
+    
+@login_required
+def decrease_item(request, pk):
+    item = get_object_or_404(Product, pk=pk)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.orderitems.filter(item=item).exists():
+            cart_item = Cart.objects.filter(item=item, user=request.user, purchased=False)[0]
+            if cart_item.quantity > 1:
+                cart_item.quantity -= 1
+                cart_item.save()
+                messages.warning(request, f"{item.name} quantity was decreased!")
+                return redirect('app_order:cart')
+            else:
+                order.orderitems.remove(cart_item) 
+                cart_item.delete() 
+                messages.warning(request, f"{item.name} was removed from cart!")             
+                return redirect('app_shop:home')
+        else:
+            messages.info(request, f"{item.name} was not in your cart!")
+            return redirect('app_shop:home')
+    else:
+        messages.info(request, "You don't have an inactive order!")
+        return redirect('app_shop:home')
